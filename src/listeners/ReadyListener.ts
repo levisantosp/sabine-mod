fb3518ff06fd7dbd76519608c926ebaa4ac137aa
levispires <contatowizard01@gmail.com>
2024-09-01 14:41:19 -0300

    fix bugs

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -45,28 +45,33 @@ export default class ReadyListener extends Listener {
     //     }
     //   ]
     // })
-    const blacklist = await Blacklist.findById('blacklist') as BlacklistSchemaInterface
     const removeUserFromBlacklist = async() => {
+      const blacklist = await Blacklist.findById('blacklist') as BlacklistSchemaInterface
       for(const user of blacklist.users.filter(user => user.endsAt !== Infinity)) {
         if((user.endsAt * 1000) < ms(ms(Date.now())!)) {
           let index = blacklist.users.findIndex(u => u.id === user.id)
           blacklist.users.splice(index, 1)
           await blacklist.save()
+          const channel = this.client.getChannel('1237496064580386917') as TextChannel
+          channel.createMessage({ content: `[Auto] - \`${(await this.client.rest.users.get(user.id)).tag}\` (\`${user.id}\`) has been unbanned from the bot.` })
         }
       }
     }
     const removeGuildFromBlacklist = async() => {
+      const blacklist = await Blacklist.findById('blacklist') as BlacklistSchemaInterface
       for(const guild of blacklist.guilds) {
         if((guild.endsAt * 1000) < ms(ms(Date.now())!)) {
           let index = blacklist.guilds.findIndex(g => g.id === guild.id)
           blacklist.guilds.splice(index, 1)
           await blacklist.save()
+          const channel = this.client.getChannel('1237496064580386917') as TextChannel
+          channel.createMessage({ content: `[Auto] - \`${guild.id}\` has been unbanned from the bot.` })
         }
       }
     }
     setInterval(async() => {
       await removeUserFromBlacklist()
       await removeGuildFromBlacklist()
-    }, 5000)
+    }, 15000)
   }
 }
