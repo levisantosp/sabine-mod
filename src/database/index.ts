6adeb925c6af2aab3e14c918b51002253aee225e
levispires <contatowizard01@gmail.com>
2024-12-07 13:56:28 -0300

    created a log for users

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
+import { client } from ".."
+import { EmbedBuilder } from "../structures"
+import { TextChannel } from "oceanic.js"
 
-export const User = mongoose.model("users", new mongoose.Schema(
+const UserSchema = mongoose.model("users", new mongoose.Schema(
   {
     _id: String,
     history: {
@@ -23,6 +26,167 @@ export const User = mongoose.model("users", new mongoose.Schema(
     warned: Boolean
   }
 ));
+export class User extends UserSchema {
+  public async get(id: string | undefined | null) {
+    return await UserSchema.findById(id) as UserSchemaInterface;
+  }
+  public async addPremium(premium: "PRO" | "BOOSTER" | "LITE" | "ULTIMATE", by: "ADD_PREMIUM_BY_COMMAND" | "BUY_PREMIUM") {
+    const keys = await Key.find() as KeySchemaInterface[];
+    let keyId: string;
+    do {
+      keyId = ""
+      const pattern = "@@@@-####-@@##-&&&&"
+      const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+      const digits = "0123456789"
+      for(const char of pattern) {
+        if(char === "@") {
+          keyId += upperCaseLetters.charAt(Math.floor(Math.random() * upperCaseLetters.length));
+        } 
+        else if(char === "#") {
+          keyId += digits.charAt(Math.floor(Math.random() * digits.length));
+        } 
+        else if(char === "&") {
+          keyId += lowerCaseLetters.charAt(Math.floor(Math.random() * lowerCaseLetters.length));
+        } 
+        else {
+          keyId += char;
+        }
+      }
+    }
+    while (keys.some(key => key.id === keyId));
+    let expiresAt = !this.plans.at(-1) ? Date.now() + 2592000000 : Date.now() + ((this.plans.length + 1) * 2592000000);
+    await new Key(
+      {
+        _id: keyId,
+        type: premium,
+        user: this.id,
+        active: false,
+        activeIn: [],
+        expiresAt,
+        canBeActivated: expiresAt - 2592000000
+      }
+    ).save();
+    this.plans.push({
+      type: premium,
+      expiresAt
+    });
+    this.warned = false;
+    await this.save();
+    const channel = client.getChannel(process.env.USERS_LOG) as TextChannel;
+    const user = client.users.get(this.id);
+    const embed = new EmbedBuilder()
+    .setTitle("New register")
+    .setDesc(`User: ${user?.mention} (${this.id})`)
+    .setFields(
+      {
+        name: by,
+        value: premium
+      }
+    );
+    const webhooks = await channel.getWebhooks();
+    let webhook = webhooks.filter(w => w.name === client.user.username + " Logger")[0];
+    if(!webhook) webhook = await channel.createWebhook({ name: client.user.username + " Logger" });
+    webhook.execute({
+      avatarURL: client.user.avatarURL(),
+      embeds: [embed]
+    });
+    return keyId;
+  }
+  public async removePremium(by: "REMOVE_PREMIUM_BY_AUTO" | "REMOVE_PREMIUM_BY_COMMAND") {
+    let premium = this.plans[0].type;
+    this.plans.shift();
+    await this.save();
+    const channel = client.getChannel(process.env.USERS_LOG) as TextChannel;
+    const user = client.users.get(this.id);
+    const embed = new EmbedBuilder()
+    .setTitle("New register")
+    .setDesc(`User: ${user?.mention} (${this.id})`)
+    .setFields(
+      {
+        name: by,
+        value: premium
+      }
+    );
+    const webhooks = await channel.getWebhooks();
+    let webhook = webhooks.filter(w => w.name === client.user.username + " Logger")[0];
+    if(!webhook) webhook = await channel.createWebhook({ name: client.user.username + " Logger" });
+    webhook.execute({
+      avatarURL: client.user.avatarURL(),
+      embeds: [embed]
+    });
+    return this as UserSchemaInterface;
+  }
+  public async addPrediction(prediction: UserSchemaHistory) {
+    this.history.push(prediction);
+    await this.save();
+    const channel = client.getChannel(process.env.USERS_LOG) as TextChannel;
+    const user = client.users.get(this.id);
+    const embed = new EmbedBuilder()
+    .setTitle("New register")
+    .setDesc(`User: ${user?.mention} (${this.id})`)
+    .setFields(
+      {
+        name: "NEW_PREDICTION",
+        value: `\`\`\`js\n${prediction}\`\`\``
+      }
+    );
+    const webhooks = await channel.getWebhooks();
+    let webhook = webhooks.filter(w => w.name === client.user.username + " Logger")[0];
+    if(!webhook) webhook = await channel.createWebhook({ name: client.user.username + " Logger" });
+    webhook.execute({
+      avatarURL: client.user.avatarURL(),
+      embeds: [embed]
+    });
+    return this as UserSchemaInterface;
+  }
+  public async addCorrectPrediction(amount: number, predictionId: string) {
+    this.guessesRight += 1;
+    await this.save();
+    const channel = client.getChannel(process.env.USERS_LOG) as TextChannel;
+    const user = client.users.get(this.id);
+    const embed = new EmbedBuilder()
+    .setTitle("New register")
+    .setDesc(`User: ${user?.mention} (${this.id})`)
+    .setFields(
+      {
+        name: "CORRECT_PREDICTION",
+        value: predictionId
+      }
+    );
+    const webhooks = await channel.getWebhooks();
+    let webhook = webhooks.filter(w => w.name === client.user.username + " Logger")[0];
+    if(!webhook) webhook = await channel.createWebhook({ name: client.user.username + " Logger" });
+    webhook.execute({
+      avatarURL: client.user.avatarURL(),
+      embeds: [embed]
+    });
+    return this as UserSchemaInterface;
+  }
+  public async addWrongPrediction(amount: number, predictionId: string) {
+    this.guessesWrong += 1;
+    await this.save();
+    const channel = client.getChannel(process.env.USERS_LOG) as TextChannel;
+    const user = client.users.get(this.id);
+    const embed = new EmbedBuilder()
+    .setTitle("New register")
+    .setDesc(`User: ${user?.mention} (${this.id})`)
+    .setFields(
+      {
+        name: "CORRECT_PREDICTION",
+        value: predictionId
+      }
+    );
+    const webhooks = await channel.getWebhooks();
+    let webhook = webhooks.filter(w => w.name === client.user.username + " Logger")[0];
+    if(!webhook) webhook = await channel.createWebhook({ name: client.user.username + " Logger" });
+    webhook.execute({
+      avatarURL: client.user.avatarURL(),
+      embeds: [embed]
+    });
+    return this as UserSchemaInterface;
+  }
+}
 export const Guild = mongoose.model("guilds", new mongoose.Schema(
   {
     _id: String,
@@ -117,7 +281,7 @@ export interface GuildSchemaInterface extends mongoose.Document {
   lastNews?: string;
   keys?: GuildSchemaKey[];
 }
-export interface UserSchemaInterface extends mongoose.Document {
+export interface UserSchemaInterface extends User {
   _id: string;
   history: UserSchemaHistory[];
   guessesRight: number;
@@ -152,4 +316,7 @@ export interface KeySchemaInterface extends mongoose.Document {
   active: boolean;
   activeIn: string[];
   canBeActivated?: number;
+}
+type Prediction = {
+
 }
